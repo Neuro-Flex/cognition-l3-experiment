09e7273e1b7da2ca3d091e731008250a2f1bc191
kasinadhsarma <kasinadhsarma@gmail.com>
2024-12-26 12:23:02 +0530

    Remove deprecated tensor type setting from another model; update default dtype and device configuration

--- models/another_model.py
+++
@@ -1,8 +0,0 @@
-
-# ...existing code...
-# Replace deprecated torch.set_default_tensor_type()
-# torch.set_default_tensor_type(torch.DoubleTensor)
-
-torch.set_default_dtype(torch.float64)
-torch.set_default_device('cuda')  # Replace 'cuda' with 'cpu' if not using GPU
-# ...existing code...
